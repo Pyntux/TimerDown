8c24e17ab86da936766832fe2a2734b59d70714c
Pyntux <pyntux@gmail.com>
2021-02-20 19:56:07 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='TimerDown',
+	version='0.2',
+	packages=[''],
+	url='https://github.com/Pyntux/TimerDown',
+	license='GPLv3',
+	author='Pyntux',
+	author_email='pyntux@gmail.com',
+	description='Small app for schedule Linux PC shutdown'
+)
